4be7e3c307fcdbde348593ca6f5639b49434c405
tonykritikos <kritikostony@gmail.com>
2022-04-12 12:14:06 +0300

    Section four

--- challenges/views.py
+++ challenges/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.template.loader import render_to_string
@@ -15,22 +15,16 @@ monthly_challenges = {
     "september": "Read Django every day!",
     "october": "Eat no meat for the month!",
     "november": "Walk 20 mins every day!",
-    "december": "Read Django every day!"
+    "december": None
 }
 
 
 # Create your views here.
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path= reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    
+    return render(request, "challenges/index.html", {"months": months})
 
 
 def monthly_challenge_by_number(request, month):
@@ -45,7 +39,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {"text": challenge_text, "month_name": month})
     except:
-        return HttpResponseNotFound("<h1>This is not a supported month!</h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
--- djangoCourseProject/settings.py
+++ djangoCourseProject/settings.py
@@ -55,6 +55,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
           #  BASE_DIR / "challenges" / "templates"
+            BASE_DIR / "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -113,6 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
